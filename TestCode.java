2e8e98b9267740538d10543d3b2f732b9d47941a
Lim Jie Han <jiehan.lim-2001@graduate.utm.my>
2024-05-27 10:10:58 +0800

    commit msg

--- TestCode.java
+++ TestCode.java
@@ -1,17 +1,17 @@
-import.java.util.ArrayList;
+import java.util.ArrayList;
 
 class Course {
     private String courseName;
     private String teacher;
     private static double instances = 0;
-    private ArrayList<Student> studentList; // Correct the declaration
+    private ArrayList<Student> studentList; // Corrected the declaration
 
     public String getCourseName(){
         return this.courseName;
     }
    
     public String getTeacher(){
-        return this.teacher; // Correct the case
+        return this.teacher; // Corrected the case
     }
 
     public Course(String courseName, String teacher){ 
@@ -20,50 +20,56 @@ class Course {
         this.teacher = teacher;
     }
 
-    public boolean addStudent(Student student){ // Add the return type
-        if (student==null || studentList.contains(student)) { // Correct the case
+    public boolean addStudent(Student student){ // Added the return type
+        if (student == null || studentList.contains(student)) { // Corrected the case
             return false;
         }
         Course.instances++;
-        studentList.add(student); // Add the parameter
+        studentList.add(student); // Added the parameter
+        return true; // Added the return statement
     }
 
     public void printStudents(){
-    for(Student s : studentList)
-            System.out.println(s.getName() + ", with " + s.getAge() + " year(s)"); // Correct the reference
-      System.out.println("Current #student "+ Course.instances);
-      
-      int y = studentList.get(3).getAge();
-      System.out.println("Youngest # "+ y + " years old");
+        for (Student s : studentList)
+            System.out.println(s.getName() + ", with " + s.getAge() + " year(s)"); // Corrected typo
+        System.out.println("Current #students: " + Course.instances);
+        
+        // Added error handling for accessing the 4th student
+        if (studentList.size() > 3) {
+            int y = studentList.get(3).getAge();
+            System.out.println("Youngest # " + y + " years old");
+        } else {
+            System.out.println("Less than 4 students in the course.");
+        }
     }  
 }  
     
-class Student{
-        private String name;
-        private int age;
+class Student {
+    private String name;
+    private int age;
  
-        public Student(String name, int age){
-            this.name = name;
-            this.age = age;
-        }
+    public Student(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
 
-        public String getName(){
-            return this.name;
-        }
+    public String getName(){
+        return this.name;
+    }
 
-        public int getAge(){
-            return this.age;
-        }
+    public int getAge(){
+        return this.age;
+    }
 }
    
-   class TestCode {// Testing code
+class TestCode { // Testing code
     public static void main(String[] args){
-        Course oop = new Course("Object Oriented Programming", "Danial Ali");
+        Course oop = new Course("Object Oriented Programming", "Danial Ali"); // Added semicolon
         oop.addStudent(new Student("Edwin Koh", 23));
         oop.addStudent(new Student("Wafia Daud", 22));
         oop.addStudent(new Student("Zafran Saad", 20));
-        System.out.println(getCourseName() + " has the following students");
+        System.out.println(oop.getCourseName() + " has the following students"); // Corrected method call
         oop.printStudents();
-        System.out.println(getCourseName() + " has the following teacher "+getTeacher());   
+        System.out.println(oop.getCourseName() + " has the following teacher " + oop.getTeacher()); // Corrected method call
     }
-   }
+}
--- TestSportFacility.java
+++ TestSportFacility.java
@@ -3,77 +3,151 @@
 // SECJ2154 - 2023/2024-2
 // Name: Lim Jie Han
 // Matric No.: A21EC3013
-
 class Address {
+    private String street;
+    private String city;
+    private String state;
+    private String postalCode;
 
-
-
-
-
-
-
-    
-}
-
-
-
-class Person {
-
-
-
-
-
-
-
-}
-
-
-
-class SportClubFacility {
-
-
-
-
-
-
-
-}
-
-class Booking {
-    
-
-
-
-
-}
-
-public class TestSportFacility{
-    public static void main(String[] args) {
-      System.out.println("\n\nLab Test - SportClub Facility Booking System\n");
-  
-      // checkpoint #1 - Create 2 sportclub facilities
-      // Display 2 sportclub facilities
-      System.out.println("Checkpoint #1");
-
-
-  
-      // checkpoint #2 - Create 1 Person member1
-      // Display member1 info 
-
-
-
-  
-  
-      // checkpoint #3 -  Create 1 Person member2
-      // Create 1 Booking booking1
-      // Display member2 info
-
-
-
-
-
-
-
-    
+    public Address(String street, String city, String state, String postalCode) {
+        this.street = street;
+        this.city = city;
+        this.state = state;
+        this.postalCode = postalCode;
     }
-  }  
+
+    @Override
+    public String toString() {
+        return street + ", " + city + ", " + state + " " + postalCode;
+    }
+}
+
+// Person class
+class Person {
+    private String name;
+    private Address address;
+
+    public Person(String name, Address address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + name + ", Address: " + address;
+    }
+}
+
+// SportClubFacility class
+class SportClubFacility {
+    private String facilityName;
+    private String facilityType;
+
+    public SportClubFacility(String facilityName, String facilityType) {
+        this.facilityName = facilityName;
+        this.facilityType = facilityType;
+    }
+
+    public String getFacilityName() {
+        return facilityName;
+    }
+
+    public String getFacilityType() {
+        return facilityType;
+    }
+
+    @Override
+    public String toString() {
+        return "Facility Name: " + facilityName + ", Facility Type: " + facilityType;
+    }
+}
+
+// Booking class
+class Booking {
+    private Person person;
+    private SportClubFacility facility;
+    private String bookingDate;
+
+    public Booking(Person person, SportClubFacility facility, String bookingDate) {
+        this.person = person;
+        this.facility = facility;
+        this.bookingDate = bookingDate;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public SportClubFacility getFacility() {
+        return facility;
+    }
+
+    public String getBookingDate() {
+        return bookingDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Booking: " + person.getName() + " booked " + facility.getFacilityName() + " on " + bookingDate;
+    }
+}
+
+// TestSportFacility class
+public class TestSportFacility {
+    public static void main(String[] args) {
+        System.out.println("\n\nLab Test - SportClub Facility Booking System\n");
+
+        // checkpoint #1 - Create 2 sportclub facilities
+        SportClubFacility facility1 = new SportClubFacility("Gymnasium", "Indoor");
+        SportClubFacility facility2 = new SportClubFacility("Swimming Pool", "Outdoor");
+
+        // Display 2 sportclub facilities
+        System.out.println("Checkpoint #1");
+        System.out.println(facility1);
+        System.out.println(facility2);
+
+        // checkpoint #2 - Create 1 Person member1
+        Address address1 = new Address("123 Main St", "CityA", "StateA", "12345");
+        Person member1 = new Person("Alice Smith", address1);
+
+        // Display member1 info
+        System.out.println("Checkpoint #2");
+        System.out.println(member1);
+
+        // checkpoint #3 - Create 1 Person member2
+        Address address2 = new Address("456 Elm St", "CityB", "StateB", "67890");
+        Person member2 = new Person("Bob Johnson", address2);
+
+        // Create 1 Booking booking1
+        Booking booking1 = new Booking(member2, facility1, "2024-06-01");
+
+        // Display member2 info
+        System.out.println("Checkpoint #3");
+        System.out.println(member2);
+        System.out.println(booking1);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
